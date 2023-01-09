0620c89d2e24608cc00aee08416a42b7c6769099
SUJAYMACHA <122225430+SUJAYMACHA@users.noreply.github.com>
2023-01-09 14:26:57 +0530

    added example D component

---
+++ src/tailwind_examples/ExampleD.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ExampleD = () => {
+  return (
+    <div className='text-3xl font-bold underline text-blue-500'>
+      hi from example d
+    </div>
+  );
+}
+
+export default ExampleD;
